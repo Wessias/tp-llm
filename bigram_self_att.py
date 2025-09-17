746d516c93b0098fa332a8c647472754948900c8
Wessias <viggowessner@gmail.com>
2025-09-17 22:23:52 +0200

    different hyperparameters

--- bigram_self_att.py
+++ bigram_self_att.py
@@ -4,17 +4,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 16 # how many independent sequences will we process in parallel?
-block_size = 32 # what is the maximum context length for predictions?
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 100
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64
-n_head = 4
-n_layer = 4
-dropout = 0.0
+n_embd = 256
+n_head = 8
+n_layer = 8
+dropout = 0.2
 # ------------
 
 with open('./data/mergedTP.txt', 'r', encoding='utf-8') as f:
