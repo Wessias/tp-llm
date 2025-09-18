14552f55f2f5e8506bfe66ebf13145d37f4d0cc0
Wessias <viggowessner@gmail.com>
2025-09-18 19:01:14 +0200

    parameter tweaks

--- bigram_self_att.py
+++ bigram_self_att.py
@@ -4,14 +4,14 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
+batch_size = 16 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000
 eval_interval = 100
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 256
+n_embd = 96
 n_head = 8
 n_layer = 8
 dropout = 0.2
